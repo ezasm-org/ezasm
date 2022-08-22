65a7ea0f122b126c5a9a71b52b3d1d28480f7fb8
CerulanLumina <CerulanLumina@users.noreply.github.com>
2022-08-21 21:48:07 -0400

    add javadoc to Instruction dispatch code

--- src/EzASM/instructions/DispatchInstruction.java
+++ src/EzASM/instructions/DispatchInstruction.java
@@ -9,22 +9,49 @@ import EzASM.parsing.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+/**
+ * An instruction which can be dispatched. This has all
+ * the information necessary to interpret a parsed {@link Line} and
+ * caches it for quick interpretation.
+ */
 public class DispatchInstruction {
 
+    /**
+     * The method that corresponds to the instruction.
+     */
     private final Method invocationTarget;
+
+    /**
+     * The parent class of the method that corresponds to the instruction
+     */
     private final Class<?> parent;
 
+    /**
+     * Create a new dispatchable instruction based on a method with specific parameters and its parent class
+     * @param parent The parent class
+     * @param invocationTarget the method for which to deduce operands for instructions and compile into a dispatchable instruction
+     */
     public DispatchInstruction(Class<?> parent, Method invocationTarget) {
         this.parent = parent;
         this.invocationTarget = invocationTarget;
     }
 
+    /**
+     * Gets the parent class of the instruction (the instruction handler)
+     * @return A Class object corresponding to the parent class
+     */
     public Class<?> getParent() {
         return parent;
     }
 
+    /**
+     * Invoke an instruction based on the parsed line (interpret the arguments and invoke the bound method)
+     * @param parent The parent instruction handler. An instance of {@link DispatchInstruction#getParent()}.
+     * @param line The parsed line to interpret
+     */
     public void invoke(Object parent, Line line) {
 
+        // TODO, change construction based on argument types. This will be necessary for deref
         Object[] args = new Object[line.getArguments().length + 1];
 
         args[args.length-1] = new RegisterOutput(line.getStoreRegister().getRegisterNumber());
--- src/EzASM/instructions/InstructionDispatcher.java
+++ src/EzASM/instructions/InstructionDispatcher.java
@@ -11,6 +11,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -18,12 +19,23 @@ import java.util.Objects;
  */
 public class InstructionDispatcher {
 
+    /**
+     * The internal backing map for Strings and loaded instructions
+     */
     private static final HashMap<String, DispatchInstruction> instructions = new HashMap<>();
 
     static {
+        // load the instructions
         registerInstructions(ArithmeticInstructions.class);
     }
 
+    /**
+     * Registers instructions from a class. Instructions are registered by
+     * scanning the class's declared methods for those annotated with
+     * {@link Instruction}. It enumerates the methods and scans their parameters
+     * to deduce the appropriate operands.
+     * @param clazz The class to register instructions from.
+     */
     public static void registerInstructions(Class<?> clazz) {
         try {
             clazz.getDeclaredConstructor(Simulator.class);
@@ -41,29 +53,60 @@ public class InstructionDispatcher {
                 .forEach(method -> registerInstruction(clazz, method));
     }
 
+    /**
+     * Registers a single instruction. The method is assumed to be annotated with {@link Instruction} at this point.
+     * This function deduces the operands based on the method's parameters (TODO)
+     * @param parent The parent class of the method
+     * @param method The method to register as an instruction
+     */
     private static void registerInstruction(Class<?> parent, Method method) {
         instructions.put(method.getName().toLowerCase(), new DispatchInstruction(parent, method));
     }
 
-    public static HashMap<String, DispatchInstruction> getInstructions() {
+    /**
+     * Retrieves the map of registered instructions.
+     * @return The Map of registered Instructions
+     */
+    public static Map<String, DispatchInstruction> getInstructions() {
+        // TODO immutable map
+        // maven library ImmutableMap.of()
         return instructions;
     }
 
+    /**
+     * Stores instances of the classes that implement the instructions. For every instantiated InstructionDispatcher,
+     * there's a set of instances that manage the instructions. This allows us to bind the Simulator to the instructions
+     */
     private final HashMap<Class<?>, Object> instructionHandlerInstances = new HashMap<>();
+
+
+    /**
+     * The bound simulator for this dispatcher.
+     */
     private final Simulator simulator;
 
+    /**
+     * Create a new Instruction Dispatcher, and bind it to an existing {@link Simulator}
+     * @param simulator The simulator to bind to
+     */
     public InstructionDispatcher(Simulator simulator) {
         this.simulator = simulator;
         loadInstructionHandlers();
     }
 
-
+    /**
+     * For all registered instructions, load an instance for each handler and bind it to a simulator
+     */
     private void loadInstructionHandlers() {
         InstructionDispatcher.instructions.values().stream()
                 .filter(instruction -> instructionHandlerInstances.get(instruction.getParent()) == null)
                 .forEach(this::loadInstructionHandler);
     }
 
+    /**
+     * Load an instance of an instruction handler for an instruction (or skip if already cached)
+     * @param instruction The registered instruction to load an instance for
+     */
     private void loadInstructionHandler(DispatchInstruction instruction) {
         try {
             Constructor<?> constructor = instruction.getParent().getDeclaredConstructor(Simulator.class);
@@ -75,6 +118,14 @@ public class InstructionDispatcher {
 
     }
 
+    /**
+     * Execute an instruction based on a parsed line.
+     * @param line The parsed line
+     * @throws InstructionDispatchException when a parsed line cannot be interpreted as a function.
+     * This could be an {@link IllegalInstructionException} if the instruction is unrecognized, or an
+     * {@link EzASM.instructions.exception.IllegalArgumentException IllegalArgumentException} if the provided parsed arguments cannot
+     * fit to the instruction (not yet implemented).
+     */
     public void execute(Line line) throws InstructionDispatchException {
         DispatchInstruction dispatch = instructions.get(line.getInstruction().getText());
         if (dispatch == null) throw new IllegalInstructionException(line.getInstruction().getText());
