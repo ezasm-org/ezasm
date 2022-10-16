fc74d49459d9abfb7a50d50c2f95e171e9904b70
Carter Bertolini <carterbertolini@gmail.com>
2022-10-16 15:58:36 -0400

    Added ANTLR generated files.

---
+++ src/main/java/com/ezasm/assembler/parser/EzASMParser.java
@@ -0,0 +1,518 @@
+// Generated from C:/Code/EzASM/src/main/java/com/ezasm/assembler\EzASM.g4 by ANTLR 4.10.1
+package com.ezasm.assembler.parser;
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class EzASMParser extends Parser {
+	static { RuntimeMetaData.checkVersion("4.10.1", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		T__0=1, T__1=2, Comment=3, Identifier=4, LabelDef=5, NewLine=6, Register=7, 
+		Decimal=8, Hex=9, Char=10, String=11, WS=12;
+	public static final int
+		RULE_prog = 0, RULE_expr = 1, RULE_argument = 2, RULE_dereference = 3, 
+		RULE_immediate = 4;
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"prog", "expr", "argument", "dereference", "immediate"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+			null, "'('", "')'"
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, null, null, "Comment", "Identifier", "LabelDef", "NewLine", "Register", 
+			"Decimal", "Hex", "Char", "String", "WS"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+	@Override
+	public String getGrammarFileName() { return "EzASM.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public EzASMParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	public static class ProgContext extends ParserRuleContext {
+		public List<TerminalNode> NewLine() { return getTokens(EzASMParser.NewLine); }
+		public TerminalNode NewLine(int i) {
+			return getToken(EzASMParser.NewLine, i);
+		}
+		public List<ExprContext> expr() {
+			return getRuleContexts(ExprContext.class);
+		}
+		public ExprContext expr(int i) {
+			return getRuleContext(ExprContext.class,i);
+		}
+		public List<TerminalNode> Comment() { return getTokens(EzASMParser.Comment); }
+		public TerminalNode Comment(int i) {
+			return getToken(EzASMParser.Comment, i);
+		}
+		public ProgContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_prog; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof EzASMListener ) ((EzASMListener)listener).enterProg(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof EzASMListener ) ((EzASMListener)listener).exitProg(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof EzASMVisitor ) return ((EzASMVisitor<? extends T>)visitor).visitProg(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final ProgContext prog() throws RecognitionException {
+		ProgContext _localctx = new ProgContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_prog);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(17);
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			while ((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << Comment) | (1L << Identifier) | (1L << LabelDef) | (1L << NewLine))) != 0)) {
+				{
+				{
+				setState(12);
+				_errHandler.sync(this);
+				switch (_input.LA(1)) {
+				case Identifier:
+				case LabelDef:
+					{
+					setState(10);
+					expr();
+					}
+					break;
+				case Comment:
+					{
+					setState(11);
+					match(Comment);
+					}
+					break;
+				case NewLine:
+					break;
+				default:
+					break;
+				}
+				setState(14);
+				match(NewLine);
+				}
+				}
+				setState(19);
+				_errHandler.sync(this);
+				_la = _input.LA(1);
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class ExprContext extends ParserRuleContext {
+		public TerminalNode Identifier() { return getToken(EzASMParser.Identifier, 0); }
+		public List<ArgumentContext> argument() {
+			return getRuleContexts(ArgumentContext.class);
+		}
+		public ArgumentContext argument(int i) {
+			return getRuleContext(ArgumentContext.class,i);
+		}
+		public TerminalNode Comment() { return getToken(EzASMParser.Comment, 0); }
+		public TerminalNode LabelDef() { return getToken(EzASMParser.LabelDef, 0); }
+		public ExprContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_expr; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof EzASMListener ) ((EzASMListener)listener).enterExpr(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof EzASMListener ) ((EzASMListener)listener).exitExpr(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof EzASMVisitor ) return ((EzASMVisitor<? extends T>)visitor).visitExpr(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final ExprContext expr() throws RecognitionException {
+		ExprContext _localctx = new ExprContext(_ctx, getState());
+		enterRule(_localctx, 2, RULE_expr);
+		int _la;
+		try {
+			setState(34);
+			_errHandler.sync(this);
+			switch (_input.LA(1)) {
+			case Identifier:
+				enterOuterAlt(_localctx, 1);
+				{
+				setState(20);
+				match(Identifier);
+				setState(24);
+				_errHandler.sync(this);
+				_la = _input.LA(1);
+				while ((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << T__0) | (1L << Identifier) | (1L << Register) | (1L << Decimal) | (1L << Hex) | (1L << Char) | (1L << String))) != 0)) {
+					{
+					{
+					setState(21);
+					argument();
+					}
+					}
+					setState(26);
+					_errHandler.sync(this);
+					_la = _input.LA(1);
+				}
+				setState(28);
+				_errHandler.sync(this);
+				_la = _input.LA(1);
+				if (_la==Comment) {
+					{
+					setState(27);
+					match(Comment);
+					}
+				}
+
+				}
+				break;
+			case LabelDef:
+				enterOuterAlt(_localctx, 2);
+				{
+				setState(30);
+				match(LabelDef);
+				setState(32);
+				_errHandler.sync(this);
+				_la = _input.LA(1);
+				if (_la==Comment) {
+					{
+					setState(31);
+					match(Comment);
+					}
+				}
+
+				}
+				break;
+			default:
+				throw new NoViableAltException(this);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class ArgumentContext extends ParserRuleContext {
+		public ImmediateContext immediate() {
+			return getRuleContext(ImmediateContext.class,0);
+		}
+		public TerminalNode Register() { return getToken(EzASMParser.Register, 0); }
+		public DereferenceContext dereference() {
+			return getRuleContext(DereferenceContext.class,0);
+		}
+		public TerminalNode Identifier() { return getToken(EzASMParser.Identifier, 0); }
+		public ArgumentContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_argument; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof EzASMListener ) ((EzASMListener)listener).enterArgument(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof EzASMListener ) ((EzASMListener)listener).exitArgument(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof EzASMVisitor ) return ((EzASMVisitor<? extends T>)visitor).visitArgument(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final ArgumentContext argument() throws RecognitionException {
+		ArgumentContext _localctx = new ArgumentContext(_ctx, getState());
+		enterRule(_localctx, 4, RULE_argument);
+		try {
+			setState(40);
+			_errHandler.sync(this);
+			switch ( getInterpreter().adaptivePredict(_input,6,_ctx) ) {
+			case 1:
+				enterOuterAlt(_localctx, 1);
+				{
+				setState(36);
+				immediate();
+				}
+				break;
+			case 2:
+				enterOuterAlt(_localctx, 2);
+				{
+				setState(37);
+				match(Register);
+				}
+				break;
+			case 3:
+				enterOuterAlt(_localctx, 3);
+				{
+				setState(38);
+				dereference();
+				}
+				break;
+			case 4:
+				enterOuterAlt(_localctx, 4);
+				{
+				setState(39);
+				match(Identifier);
+				}
+				break;
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class DereferenceContext extends ParserRuleContext {
+		public TerminalNode Register() { return getToken(EzASMParser.Register, 0); }
+		public ImmediateContext immediate() {
+			return getRuleContext(ImmediateContext.class,0);
+		}
+		public DereferenceContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_dereference; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof EzASMListener ) ((EzASMListener)listener).enterDereference(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof EzASMListener ) ((EzASMListener)listener).exitDereference(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof EzASMVisitor ) return ((EzASMVisitor<? extends T>)visitor).visitDereference(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final DereferenceContext dereference() throws RecognitionException {
+		DereferenceContext _localctx = new DereferenceContext(_ctx, getState());
+		enterRule(_localctx, 6, RULE_dereference);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(43);
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			if ((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << Decimal) | (1L << Hex) | (1L << Char) | (1L << String))) != 0)) {
+				{
+				setState(42);
+				immediate();
+				}
+			}
+
+			setState(45);
+			match(T__0);
+			setState(46);
+			match(Register);
+			setState(47);
+			match(T__1);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class ImmediateContext extends ParserRuleContext {
+		public TerminalNode Hex() { return getToken(EzASMParser.Hex, 0); }
+		public TerminalNode Decimal() { return getToken(EzASMParser.Decimal, 0); }
+		public TerminalNode Char() { return getToken(EzASMParser.Char, 0); }
+		public TerminalNode String() { return getToken(EzASMParser.String, 0); }
+		public ImmediateContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_immediate; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof EzASMListener ) ((EzASMListener)listener).enterImmediate(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof EzASMListener ) ((EzASMListener)listener).exitImmediate(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof EzASMVisitor ) return ((EzASMVisitor<? extends T>)visitor).visitImmediate(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final ImmediateContext immediate() throws RecognitionException {
+		ImmediateContext _localctx = new ImmediateContext(_ctx, getState());
+		enterRule(_localctx, 8, RULE_immediate);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(49);
+			_la = _input.LA(1);
+			if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << Decimal) | (1L << Hex) | (1L << Char) | (1L << String))) != 0)) ) {
+			_errHandler.recoverInline(this);
+			}
+			else {
+				if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
+				_errHandler.reportMatch(this);
+				consume();
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static final String _serializedATN =
+		"\u0004\u0001\f4\u0002\u0000\u0007\u0000\u0002\u0001\u0007\u0001\u0002"+
+		"\u0002\u0007\u0002\u0002\u0003\u0007\u0003\u0002\u0004\u0007\u0004\u0001"+
+		"\u0000\u0001\u0000\u0003\u0000\r\b\u0000\u0001\u0000\u0005\u0000\u0010"+
+		"\b\u0000\n\u0000\f\u0000\u0013\t\u0000\u0001\u0001\u0001\u0001\u0005\u0001"+
+		"\u0017\b\u0001\n\u0001\f\u0001\u001a\t\u0001\u0001\u0001\u0003\u0001\u001d"+
+		"\b\u0001\u0001\u0001\u0001\u0001\u0003\u0001!\b\u0001\u0003\u0001#\b\u0001"+
+		"\u0001\u0002\u0001\u0002\u0001\u0002\u0001\u0002\u0003\u0002)\b\u0002"+
+		"\u0001\u0003\u0003\u0003,\b\u0003\u0001\u0003\u0001\u0003\u0001\u0003"+
+		"\u0001\u0003\u0001\u0004\u0001\u0004\u0001\u0004\u0000\u0000\u0005\u0000"+
+		"\u0002\u0004\u0006\b\u0000\u0001\u0001\u0000\b\u000b9\u0000\u0011\u0001"+
+		"\u0000\u0000\u0000\u0002\"\u0001\u0000\u0000\u0000\u0004(\u0001\u0000"+
+		"\u0000\u0000\u0006+\u0001\u0000\u0000\u0000\b1\u0001\u0000\u0000\u0000"+
+		"\n\r\u0003\u0002\u0001\u0000\u000b\r\u0005\u0003\u0000\u0000\f\n\u0001"+
+		"\u0000\u0000\u0000\f\u000b\u0001\u0000\u0000\u0000\f\r\u0001\u0000\u0000"+
+		"\u0000\r\u000e\u0001\u0000\u0000\u0000\u000e\u0010\u0005\u0006\u0000\u0000"+
+		"\u000f\f\u0001\u0000\u0000\u0000\u0010\u0013\u0001\u0000\u0000\u0000\u0011"+
+		"\u000f\u0001\u0000\u0000\u0000\u0011\u0012\u0001\u0000\u0000\u0000\u0012"+
+		"\u0001\u0001\u0000\u0000\u0000\u0013\u0011\u0001\u0000\u0000\u0000\u0014"+
+		"\u0018\u0005\u0004\u0000\u0000\u0015\u0017\u0003\u0004\u0002\u0000\u0016"+
+		"\u0015\u0001\u0000\u0000\u0000\u0017\u001a\u0001\u0000\u0000\u0000\u0018"+
+		"\u0016\u0001\u0000\u0000\u0000\u0018\u0019\u0001\u0000\u0000\u0000\u0019"+
+		"\u001c\u0001\u0000\u0000\u0000\u001a\u0018\u0001\u0000\u0000\u0000\u001b"+
+		"\u001d\u0005\u0003\u0000\u0000\u001c\u001b\u0001\u0000\u0000\u0000\u001c"+
+		"\u001d\u0001\u0000\u0000\u0000\u001d#\u0001\u0000\u0000\u0000\u001e \u0005"+
+		"\u0005\u0000\u0000\u001f!\u0005\u0003\u0000\u0000 \u001f\u0001\u0000\u0000"+
+		"\u0000 !\u0001\u0000\u0000\u0000!#\u0001\u0000\u0000\u0000\"\u0014\u0001"+
+		"\u0000\u0000\u0000\"\u001e\u0001\u0000\u0000\u0000#\u0003\u0001\u0000"+
+		"\u0000\u0000$)\u0003\b\u0004\u0000%)\u0005\u0007\u0000\u0000&)\u0003\u0006"+
+		"\u0003\u0000\')\u0005\u0004\u0000\u0000($\u0001\u0000\u0000\u0000(%\u0001"+
+		"\u0000\u0000\u0000(&\u0001\u0000\u0000\u0000(\'\u0001\u0000\u0000\u0000"+
+		")\u0005\u0001\u0000\u0000\u0000*,\u0003\b\u0004\u0000+*\u0001\u0000\u0000"+
+		"\u0000+,\u0001\u0000\u0000\u0000,-\u0001\u0000\u0000\u0000-.\u0005\u0001"+
+		"\u0000\u0000./\u0005\u0007\u0000\u0000/0\u0005\u0002\u0000\u00000\u0007"+
+		"\u0001\u0000\u0000\u000012\u0007\u0000\u0000\u00002\t\u0001\u0000\u0000"+
+		"\u0000\b\f\u0011\u0018\u001c \"(+";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
