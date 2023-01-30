3cba66c16fe9cde6c1e3570c53bec1ba2a65d4f5
Wyatt Ross <2treecko@gmail.com>
2023-01-30 17:44:35 -0500

    added Config class for handling configurations

---
+++ src/main/java/com/ezasm/Config.java
@@ -0,0 +1,93 @@
+package com.ezasm;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+    
+    /**
+     * A path to the config file for EzASM
+     */
+    public static final String CONFIG_PATH = System.getProperty("user.home") + File.separator + ".ezasmrc";
+
+    /**
+     * The configuration file for EzASM
+     */
+    public static final File CONFIG_FILE = new File(CONFIG_PATH);
+
+    // All of the names of the configuration settings available to the user
+    public static final String FONT_SIZE = "FONT_SIZE";
+    public static final String SIMULATION_SPEED = "SIMULATION_SPEED";
+    public static final String THEME = "THEME";
+    
+    // All of EzASM's defaults
+    public static final String DEFAULT_FONT_SIZE = "12";
+    public static final String DEFAULT_SIMULATION_SPEED = "500";
+    public static final String DEFAULT_THEME = "Light";
+    
+    private Properties props = new Properties();
+
+    public Config() {
+        if (CONFIG_FILE.exists()){
+            readProperties();
+        }
+        else{
+            props.setProperty(FONT_SIZE, DEFAULT_FONT_SIZE);
+            props.setProperty(SIMULATION_SPEED, DEFAULT_SIMULATION_SPEED);
+            props.setProperty(THEME, DEFAULT_THEME);
+            saveChanges();
+        }
+    }
+
+    public int getFontSize(){
+        return Integer.parseInt(props.getProperty(FONT_SIZE));
+    }
+
+    public void setFontSize(int size){
+        props.setProperty(FONT_SIZE, String.valueOf(size));
+    }
+
+    public String getTheme(){
+        return props.getProperty(THEME);
+    }
+
+    public void setTheme(String newTheme){
+        props.setProperty(THEME, newTheme);
+    }
+
+    public int getSimSpeed(){
+        return Integer.parseInt(props.getProperty(SIMULATION_SPEED));
+    }
+
+    public void setSimSpeed(int speed){
+        props.setProperty(SIMULATION_SPEED, String.valueOf(speed));
+    }
+
+    public void saveChanges(){
+        try{
+            FileWriter writer = new FileWriter(CONFIG_FILE);
+            props.store(writer, "");
+            writer.close();
+        }
+        catch(IOException e){
+            System.out.println("ERROR SAVING SETTING");
+        }
+    }
+
+    public Properties readProperties() {
+        Properties properties = new Properties();
+        try{
+            FileInputStream propReader = new FileInputStream(CONFIG_FILE);
+            properties.load(propReader);
+            propReader.close();
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+        return properties;
+    }
+
+}
