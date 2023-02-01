5d16c6bc6672a130ce0d903f99313a3741a99c99
Wyatt Ross <2treecko@gmail.com>
2023-02-01 16:27:02 -0500

    Saving works. Plus some other stuff

--- src/main/java/com/ezasm/Config.java
+++ src/main/java/com/ezasm/Config.java
@@ -5,18 +5,20 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Vector;
+import java.util.Arrays;
 
 public class Config {
     
     /**
      * A path to the config file for EzASM
      */
-    public static final String CONFIG_PATH = System.getProperty("user.home") + File.separator + ".ezasmrc";
+    private static final String CONFIG_PATH = System.getProperty("user.home") + File.separator + ".ezasmrc";
 
     /**
      * The configuration file for EzASM
      */
-    public static final File CONFIG_FILE = new File(CONFIG_PATH);
+    private static final File CONFIG_FILE = new File(CONFIG_PATH);
 
     // All of the names of the configuration settings available to the user
     public static final String FONT_SIZE = "FONT_SIZE";
@@ -25,8 +27,12 @@ public class Config {
     
     // All of EzASM's defaults
     public static final String DEFAULT_FONT_SIZE = "12";
-    public static final String DEFAULT_SIMULATION_SPEED = "500";
+    public static final String DEFAULT_SIMULATION_SPEED = "250";
     public static final String DEFAULT_THEME = "Light";
+
+    // Possible themes
+    private static final String[] THEMEARRAY = {"Light", "Dark", "Purple"};
+    public static final Vector<String> THEMES = new Vector<String>(Arrays.asList(THEMEARRAY));
     
     private Properties props = new Properties();
 
@@ -64,6 +70,12 @@ public class Config {
 
     public void setSimSpeed(int speed){
         props.setProperty(SIMULATION_SPEED, String.valueOf(speed));
+    }
+
+    public void resetDefaults(){
+        this.setTheme(DEFAULT_THEME);
+        this.setFontSize(Integer.parseInt(DEFAULT_FONT_SIZE));
+        this.setSimSpeed(Integer.parseInt(DEFAULT_SIMULATION_SPEED));
     }
 
     public void saveChanges(){
--- src/main/java/com/ezasm/gui/SettingsPopup.java
+++ src/main/java/com/ezasm/gui/SettingsPopup.java
@@ -3,6 +3,9 @@ package com.ezasm.gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -30,6 +33,9 @@ public class SettingsPopup {
 
     private Config config;
 
+    private ButtonActionListener buttonActionListener; 
+
+
     protected SettingsPopup() {
         instance = this;
         config = new Config();
@@ -49,11 +55,15 @@ public class SettingsPopup {
     }
 
     private void initialize(){
+        buttonActionListener = new ButtonActionListener(config);
         layout = new BorderLayout();
         popup = new JFrame("EzASM Configurator");
         popup.setLayout(layout);
         popup.setMinimumSize(new Dimension(500,300));
         popup.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        themeLabel = new JLabel(THEME);
+        themeInput = new JComboBox<String>(Config.THEMES);
 
         fontSizeLabel = new JLabel(FONTSIZE);
         speedLabel = new JLabel(SIMULATION_SPEED);
@@ -68,8 +78,8 @@ public class SettingsPopup {
         grid.add(fontInput);
         grid.add(speedLabel);
         grid.add(speedSlider);
-        // grid.add(themeLabel);
-        // grid.add(themeInput);
+        grid.add(themeLabel);
+        grid.add(themeInput);
         
         save = new JButton(SAVE);
         resetDefaults = new JButton(RESET);
@@ -77,10 +87,41 @@ public class SettingsPopup {
         grid.add(save);
         grid.add(resetDefaults);
 
+        resetDefaults.addActionListener(buttonActionListener);
+        save.addActionListener(buttonActionListener);
+
         popup.add(grid, BorderLayout.CENTER);
 
         popup.validate();
         popup.pack();
         popup.setVisible(true);
+    }
+
+    private static class ButtonActionListener implements ActionListener{
+        private Config config;
+
+        public ButtonActionListener(Config cfg){
+            super();
+            config = cfg;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String action = e.getActionCommand();
+            SettingsPopup instance = SettingsPopup.getInstance();
+            if (action.startsWith("Save")){
+                instance.config.setFontSize(Integer.parseInt(instance.fontInput.getText()));
+                instance.config.setSimSpeed(instance.speedSlider.getX());
+                instance.config.setTheme(instance.themeInput.getSelectedItem().toString());
+                instance.config.saveChanges();
+            }
+            if (action.startsWith("Reset")){
+                instance.config.resetDefaults();
+                instance.fontInput.setText(Config.DEFAULT_FONT_SIZE);
+                instance.speedSlider.setValue(Integer.parseInt(Config.DEFAULT_SIMULATION_SPEED));
+                instance.themeInput.setSelectedIndex(0);
+            }
+        }
+
     }
 }
