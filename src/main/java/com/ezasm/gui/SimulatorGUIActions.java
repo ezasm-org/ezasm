cf5ac9d720e295b4a899c592c63b93764b3882c4
trevorbrunette <trevorbrunette@gmail.com>
2023-02-07 17:44:41 -0500

    Added capacity to start and stop the program

--- src/main/java/com/ezasm/gui/SimulatorGUIActions.java
+++ src/main/java/com/ezasm/gui/SimulatorGUIActions.java
@@ -14,8 +14,11 @@ public class SimulatorGUIActions {
         PAUSED
     }
 
+    private static final long LOOP_BUSY_WAIT_MS = 50L;
+
     private static Thread worker;
     private static State state = State.IDLE;
+    private static long instructionDelayMS = 500L;
 
     private static void setState(State newState) {
         state = newState;
@@ -31,71 +34,119 @@ public class SimulatorGUIActions {
         resetButton.setEnabled(state != State.IDLE);
     }
 
-    static void handleProgramCompletion() {
-        Window.getInstance().handleProgramCompletion();
+    private static void handleProgramCompletion() {
+        if (state != State.IDLE) {
+            Window.getInstance().handleProgramCompletion();
+        }
     }
 
-    public static void step() {
-        if (Window.getInstance().getEditable()) {
-            Window.getInstance().setEditable(false);
+    static void step() {
+        if (state == State.IDLE || state == State.STOPPED) {
+            setState(State.PAUSED);
             try {
                 Window.getInstance().parseText();
                 System.out.println("** Program starting **");
+                startWorker();
             } catch (ParseException e) {
                 setState(State.IDLE);
                 Window.getInstance().setEditable(true);
                 Window.getInstance().handleParseException(e);
             }
-        }
-        if (Window.getInstance().getSimulator().isDone()) {
-            setState(State.STOPPED);
-            handleProgramCompletion();
         } else {
             try {
                 Window.getInstance().getSimulator().executeLineFromPC();
-                setState(State.PAUSED);
+                Window.updateAll();
             } catch (SimulationException e) {
                 setState(State.STOPPED);
                 System.err.println(e.getMessage());
                 handleProgramCompletion();
             }
         }
+        if (Window.getInstance().getSimulator().isDone() && state == State.PAUSED) {
+            setState(State.STOPPED);
+        }
     }
 
-    public static void start() {
+    static void start() {
         try {
             Window.getInstance().setEditable(false);
             Window.getInstance().parseText();
             setState(State.RUNNING);
             System.out.println("** Program starting **");
+            startWorker();
         } catch (ParseException e) {
             stop();
             Window.getInstance().handleParseException(e);
         }
     }
 
-    public static void stop() {
+    static void stop() {
+        setState(State.STOPPED);
+        killWorker();
+        awaitWorkerTermination();
+        handleProgramCompletion();
+    }
+
+    static void pause() {
+        setState(State.PAUSED);
+    }
+
+    static void resume() {
+        setState(State.RUNNING);
+    }
+
+    static void reset() {
+        if (state != State.STOPPED) {
+            killWorker();
+            awaitWorkerTermination();
+            handleProgramCompletion();
+        }
+        setState(State.IDLE);
+        Window.getInstance().getSimulator().resetAll();
+        Window.updateAll();
+    }
+
+    private static void simulationLoop() {
+        while (!Window.getInstance().getSimulator().isDone() && (state == State.RUNNING || state == State.PAUSED)) {
+            try {
+                Window.getInstance().getSimulator().executeLineFromPC();
+                Window.updateAll();
+            } catch (SimulationException e) {
+                Window.getInstance().handleParseException(e);
+                setState(State.STOPPED);
+                break;
+            }
+            try {
+                Thread.sleep(instructionDelayMS);
+                while (state == State.PAUSED) { // busy wait
+                    Thread.sleep(LOOP_BUSY_WAIT_MS);
+                }
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+        onSimulatorCompleteInLoop();
+    }
+
+    private static void onSimulatorCompleteInLoop() {
         setState(State.STOPPED);
         handleProgramCompletion();
     }
 
-    public static void pause() {
-        setState(State.PAUSED);
+    private static void startWorker() {
+        worker = new Thread(SimulatorGUIActions::simulationLoop);
+        worker.start();
     }
 
-    public static void resume() {
-        setState(State.RUNNING);
+    private static void killWorker() {
+        worker.interrupt();
     }
 
-    public static void reset() {
-        handleProgramCompletion();
-        setState(State.IDLE);
-
-        //Window.getInstance().getSimulationThread().interrupt();
-        //Window.getInstance().getSimulationThread().awaitTermination();
-
-        Window.getInstance().getSimulator().resetAll();
-        Window.updateAll();
+    private static void awaitWorkerTermination() {
+        try {
+            worker.join();
+        } catch (InterruptedException ignored) {
+        }
     }
 
 }
