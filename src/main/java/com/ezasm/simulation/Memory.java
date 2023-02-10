e21bcf6d9032f3268bbf8e2fd7bd6aaf3990028f
trevorbrunette <trevorbrunette@gmail.com>
2023-02-10 16:56:31 -0500

    Fixed readString

--- src/main/java/com/ezasm/simulation/Memory.java
+++ src/main/java/com/ezasm/simulation/Memory.java
@@ -178,20 +178,19 @@ public class Memory {
         if (maxSize < 0) {
             throw new SimulationException(String.format("String size cannot be %d", maxSize));
         }
-        if (address < 0 || (address - OFFSET + maxSize) >= this.MEMORY_SIZE) {
-            throw new SimulationAddressOutOfBoundsException(address + OFFSET);
+        if (address - OFFSET < 0 || (address + maxSize - OFFSET) >= this.MEMORY_SIZE) {
+            throw new SimulationAddressOutOfBoundsException(address);
         }
 
-        String toString = "";
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < maxSize; ++i) {
-            char read = (char) Conversion.bytesToLong(read(address + (i * WORD_SIZE)));
-            toString += read;
-            if (read == '\0') {
-                System.out.println(i);
+            char c = (char) Conversion.bytesToLong(read(address + i * WORD_SIZE));
+            if (c == '\0') {
                 break;
             }
+            sb.append(c);
         }
-        return toString;
+        return sb.toString();
     }
 
     /**
@@ -222,24 +221,27 @@ public class Memory {
      * Writes a String to the specified address.
      *
      * @param address the address to write at.
-     * @param data    the String to write.
+     * @param string    the String to write.
      * @param maxSize the maximum size of the String to be in bytes.
      */
-    public void writeString(int address, String data, int maxSize) throws SimulationException {
-        address = address - OFFSET;
+    public void writeString(int address, String string, int maxSize) throws SimulationException {
+        int realAddress = address - OFFSET;
+
         if (maxSize < 0) {
             throw new SimulationException(String.format("String size cannot be %d", maxSize));
         }
-        if (address < 0 || (address + data.getBytes().length) >= this.MEMORY_SIZE) {
-            throw new SimulationAddressOutOfBoundsException(address + OFFSET);
+        if (realAddress < 0 || (realAddress + string.length()) >= this.MEMORY_SIZE) {
+            throw new SimulationAddressOutOfBoundsException(address);
         }
 
-        for (int i = 0; i < data.getBytes().length && i < maxSize; ++i) {
-            memory[address + i] = data.getBytes()[i];
+        byte[][] data = Conversion.stringToBytes(string);
+
+        for (int i = 0; i < data.length && i < maxSize; ++i) {
+            write(address + i * WORD_SIZE, data[i]);
         }
 
-        if (maxSize <= data.getBytes().length) {
-            memory[address + maxSize] = '\0';
+        if (maxSize <= data.length) {
+            write(address + maxSize * WORD_SIZE, Conversion.longToBytes('\0'));
         }
     }
 
