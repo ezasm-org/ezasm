5eeca4382c72b3ac32a3280583a6d617c738244c
Wyatt Ross <2treecko@gmail.com>
2023-02-15 00:14:02 -0500

    Io redirection (#78)
    * Shockingly painless
    
    * now makes a file if it isn't already there for output
    
    * fixed import error. Hopefully tests work now
    
    * fixed import error. Hopefully tests work now
    
    * removed print
    
    * Better error message
    
    * Better error msg
    
    * The same but better
    
    * Linter ratio
    
    ---------
    
    Co-authored-by: trevorbrunette <trevorbrunette@gmail.com>

--- src/main/java/com/ezasm/util/CommandLineInterface.java
+++ src/main/java/com/ezasm/util/CommandLineInterface.java
@@ -1,5 +1,6 @@
 package com.ezasm.util;
 
+import com.ezasm.instructions.implementation.TerminalInstructions;
 import com.ezasm.parsing.Lexer;
 import com.ezasm.parsing.Line;
 import com.ezasm.parsing.ParseException;
@@ -7,7 +8,11 @@ import com.ezasm.simulation.ISimulator;
 import com.ezasm.simulation.exception.SimulationException;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Scanner;
 
 /**
@@ -18,6 +23,8 @@ public class CommandLineInterface {
 
     private final ISimulator simulator;
     private final boolean cli;
+    private InputStream inputStream = null;
+    private OutputStream outputStream = null;
 
     /**
      * Constructs a basic CLI based on the given Simulator. This CLI will read from the terminal until the program is
@@ -48,9 +55,53 @@ public class CommandLineInterface {
     }
 
     /**
+     * Constructs a CLI based ont he given Simulator for operating code from a file with redirected input and/or output
+     *
+     * @param simulator      the given Simulator.
+     * @param file           the file to read code from.
+     * @param inputFilePath  the file to read the input from.
+     * @param outputFilePath the file to write the output to.
+     */
+    public CommandLineInterface(ISimulator simulator, String file, String inputFilePath, String outputFilePath) {
+        this.simulator = simulator;
+        this.cli = false;
+        try {
+            this.simulator.addLines(Lexer.parseLines(FileIO.readFile(new File(file)), 0));
+        } catch (ParseException | IOException e) {
+            System.err.println("Unable to parse the given file: " + e.getMessage());
+            System.exit(1);
+        }
+
+        try {
+            if (inputFilePath.length() > 0) {
+                inputStream = new FileInputStream(new File(inputFilePath));
+            } else {
+                inputStream = System.in;
+            }
+        } catch (IOException e) {
+            System.err.printf("Unable to read input from %s: %s", inputFilePath, e.getMessage());
+            System.exit(1);
+        }
+
+        try {
+            if (outputFilePath.length() > 0) {
+                File outputFile = new File(outputFilePath);
+                outputFile.createNewFile();
+                outputStream = new FileOutputStream(outputFile);
+            } else
+                outputStream = System.out;
+        } catch (IOException e) {
+            System.err.printf("Unable to write output to %s: %s", outputFilePath, e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    /**
      * Begins the simulation. Starts reading CLI input or reads and executes from the given file.
      */
     public void startSimulation() {
+        if (inputStream != null || outputStream != null)
+            TerminalInstructions.setInputOutput(inputStream, outputStream);
         if (cli) {
             runFromCliInput();
         } else {
@@ -71,7 +122,6 @@ public class CommandLineInterface {
                 Line line = Lexer.parseLine(scanner.nextLine(), lineNumber);
                 simulator.runLine(line);
             } catch (ParseException | SimulationException e) {
-                System.err.println(e.getMessage());
                 System.err.flush();
             }
             System.out.print("> ");
