8300ec70eaf7842bf013722af4c0f2400869e2a2
clbertolini <carterbertolini@gmail.com>
2023-02-17 17:13:15 -0500

    Finished support for multiple instructions with the same name.

--- src/main/java/com/ezasm/gui/editor/EzTokenMaker.java
+++ src/main/java/com/ezasm/gui/editor/EzTokenMaker.java
@@ -1,7 +1,6 @@
 package com.ezasm.gui.editor;
 
 import com.ezasm.instructions.InstructionDispatcher;
-import com.ezasm.instructions.InstructionPrototype;
 import com.ezasm.parsing.Lexer;
 import org.fife.ui.rsyntaxtextarea.*;
 
@@ -32,8 +31,8 @@ public class EzTokenMaker extends AbstractTokenMaker {
     @Override
     public TokenMap getWordsToHighlight() {
         TokenMap tokenMap = new TokenMap();
-        for (InstructionPrototype instruction : InstructionDispatcher.getInstructions().keySet()) {
-            tokenMap.put(instruction.name(), Token.RESERVED_WORD);
+        for (String instruction : InstructionDispatcher.getInstructions().keySet()) {
+            tokenMap.put(instruction, Token.RESERVED_WORD);
         }
         return tokenMap;
     }
