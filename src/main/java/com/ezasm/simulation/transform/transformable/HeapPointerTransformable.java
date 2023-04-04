c0c5a408f9848da7049f1fdf8cb39db5682d2a14
clbertolini <carterbertolini@gmail.com>
2023-04-04 16:06:17 -0400

    Added SimulationOutOfMemoryException that is thrown when alloc runs out of space (stack collides with heap).

--- src/main/java/com/ezasm/simulation/transform/transformable/HeapPointerTransformable.java
+++ src/main/java/com/ezasm/simulation/transform/transformable/HeapPointerTransformable.java
@@ -1,6 +1,8 @@
 package com.ezasm.simulation.transform.transformable;
 
+import com.ezasm.simulation.Registers;
 import com.ezasm.simulation.Simulator;
+import com.ezasm.simulation.exception.SimulationOutOfMemoryException;
 import com.ezasm.util.RawData;
 
 /**
@@ -33,7 +35,10 @@ public class HeapPointerTransformable extends AbstractTransformableInput {
      * @param value the heap pointer's new value.
      */
     @Override
-    public void set(RawData value) {
+    public void set(RawData value) throws SimulationOutOfMemoryException {
+        if (simulator.getRegisters().getRegister(Registers.SP).getLong() <= value.intValue()) {
+            throw new SimulationOutOfMemoryException(value.intValue() - simulator.getMemory().currentHeapPointer());
+        }
         simulator.getMemory().setHeapPointer((int) value.intValue());
     }
 }
