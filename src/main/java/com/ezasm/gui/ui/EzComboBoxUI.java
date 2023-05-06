262a50db7dca8a22a4d7f6bb16ea8be4546268a6
Trevor Brunette <trevorbrunette@gmail.com>
2023-05-06 12:43:07 -0400

    More javadocs

--- src/main/java/com/ezasm/gui/ui/EzComboBoxUI.java
+++ src/main/java/com/ezasm/gui/ui/EzComboBoxUI.java
@@ -5,15 +5,30 @@ import com.ezasm.gui.util.EditorTheme;
 import javax.swing.plaf.basic.BasicComboBoxUI;
 import java.awt.*;
 
+/**
+ * Represents UI theming for a combo-box GUI element.
+ */
 public class EzComboBoxUI extends BasicComboBoxUI {
 
     private final EditorTheme editorTheme;
 
+    /**
+     * Constructs the combobox with a given theme.
+     *
+     * @param editorTheme the theme to use.
+     */
     public EzComboBoxUI(EditorTheme editorTheme) {
         super();
         this.editorTheme = editorTheme;
     }
 
+    /**
+     * Paints the combo box.
+     *
+     * @param g        an instance of {@code Graphics}.
+     * @param bounds   a bounding rectangle to render to.
+     * @param hasFocus is focused.
+     */
     @Override
     public void paintCurrentValueBackground(Graphics g, Rectangle bounds, boolean hasFocus) {
         super.paintCurrentValueBackground(g, bounds, hasFocus);
