df74de63b07d74c9ca7a8781477be225373b9bd8
Vanessa <hwvanessa14@gmail.com>
2025-11-11 19:15:10 -0500

    Simplified AlternatingHighlightTable to prevent reused code snippets

--- src/main/java/com/ezasm/gui/table/AlternatingHighlightTable.java
+++ src/main/java/com/ezasm/gui/table/AlternatingHighlightTable.java
@@ -6,8 +6,7 @@ import java.awt.Font;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 
 import com.ezasm.gui.util.EditorTheme;
 import com.ezasm.gui.util.IThemeable;
@@ -16,61 +15,34 @@ import com.ezasm.gui.util.IThemeable;
  * An extension of JTable with colors that alternate based on whether a row has an even index or an odd index, and
  * highlights specific rows
  */
-public class AlternatingHighlightTable extends JTable implements IThemeable {
+public class AlternatingHighlightTable extends AlternatingColorTable implements IThemeable {
 
-    private Color evens;
-    private Color odds;
     private Color highlight;
     private Color changedTextColor;
     private final Set<Integer> flashingRows = new HashSet<>();
 
     public AlternatingHighlightTable(EditorTheme theme) {
-        super();
-        installRenderer();
-        setSelectionBackground(evens);
-        setSelectionForeground(theme.foreground());
+        super(theme);
     }
 
-    private void installRenderer() {
-        setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
-            private final DefaultTableCellRenderer defaultRenderer = new DefaultTableCellRenderer();
+    @Override
+    public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+        Component component = super.prepareRenderer(renderer, row, column);
 
-            @Override
-            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-                    boolean hasFocus, int row, int column) {
-                Component comp = defaultRenderer.getTableCellRendererComponent(table, value, false, hasFocus, row,
-                        column);
+        if (flashingRows.contains(row)) {
+            component.setBackground(highlight);
+            component.setForeground(changedTextColor);
+        } else {
+            component.setForeground(this.getForeground());
+        }
 
-                if (comp instanceof DefaultTableCellRenderer) {
-                    ((DefaultTableCellRenderer) comp).setOpaque(true);
-                }
-
-                // Background logic
-                if (flashingRows.contains(row)) {
-                    comp.setBackground(highlight);
-                    comp.setForeground(changedTextColor);
-                } else {
-                    comp.setBackground(row % 2 == 0 ? evens : odds);
-                    comp.setForeground(table.getForeground());
-
-                }
-
-                return comp;
-            }
-        });
+        return component;
     }
 
     @Override
     public void applyTheme(Font font, EditorTheme theme) {
-        EditorTheme.applyFontTheme(this, font, theme);
-        EditorTheme.applyFontTheme(tableHeader, font, theme);
+        super.applyTheme(font, theme);
 
-        TableHeaderRenderer newTableHeaderRenderer = new TableHeaderRenderer(this);
-        newTableHeaderRenderer.applyTheme(font, theme);
-        getTableHeader().setDefaultRenderer(newTableHeaderRenderer);
-
-        this.evens = theme.currentLine();
-        this.odds = theme.background();
         this.highlight = theme.yellow();
         this.changedTextColor = Color.RED;
         repaint();
